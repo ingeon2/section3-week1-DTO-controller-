715b537e94d5c3c8be65bd844b4cf29a74e050a2
ingeon <dlsrjsld@naver.com>
2023-02-23 00:23:44 +0900

    data.sql 에 아무것도 작성하지 않으면 실행시 DI 에러 나타남.

---
+++ src/main/generated/com/solostudy/order/mapper/OrderMapperImpl.java
@@ -0,0 +1,13 @@
+package com.solostudy.order.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-02-22T21:18:17+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class OrderMapperImpl implements OrderMapper {
+}
